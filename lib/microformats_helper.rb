94d0334d1b691c2ee76f83e9c3bea0e31edd8710
arnodmental <arnodmental@gmail.com>
2011-08-12 14:23:03 +0200

    updated lib according to http://stackoverflow.com/questions/6551822/create-a-simple-rails-3-text-helper-gem

--- lib/microformats_helper.rb
+++ lib/microformats_helper.rb
@@ -1,4 +1,5 @@
-module MicroformatsHelper
-  extend ActiveSupport::Autoload
-  autoload :Helpers  
+require 'microformats_helper/helpers'
+
+ActiveSupport.on_load(:action_view) do
+  include MicroformatsHelper::Helpers
 end
