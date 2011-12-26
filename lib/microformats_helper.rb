0bcb2bf6096a56b80a97be4a07e532a85b89a8a3
Ricardo Shiota Yasuda <shadow11@gmail.com>
2011-12-26 16:52:09 -0200

    Rails 2.x backwards compatibility

--- lib/microformats_helper.rb
+++ lib/microformats_helper.rb
@@ -1,6 +1,10 @@
 require 'microformats_helper/helpers'
 require 'active_support'
 
-ActiveSupport.on_load(:action_view) do
-  include MicroformatsHelper::Helpers
+if ActiveSupport.respond_to?(:on_load)
+  ActiveSupport.on_load(:action_view) do
+    include MicroformatsHelper::Helpers
+  end
+else
+  ActionView::Base.send :include, MicroformatsHelper::Helpers
 end
